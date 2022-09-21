214b1b6cd4feae8d6b275f5d57445f918eb6727d
Sachin <sachinbochare94@gmail.com>
2022-09-21 16:40:39 +0530

    done

---
+++ UserCRUD/src/main/java/com/example/demo/Model/Pallavi.java
@@ -0,0 +1,19 @@
+package com.example.demo.Model;
+
+public class Pallavi {
+	private String FirstName;
+	private String LastName;
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+}
